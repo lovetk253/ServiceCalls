72b8b3b6125278918bd9d7cbe38cf37201390555
nguyenkma <lovetk253@gmail.com>
2018-10-02 15:33:43 +0700

    Service Calls

---
+++ src/main/java/org/demo/Baitap1RunApplication.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap1RunApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Baitap1RunApplication.class, args);
+		
+	}
+}
